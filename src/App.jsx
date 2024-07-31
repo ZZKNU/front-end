f6ec4a01124d36bca489e3d11c018d2f7522ea6c
kimgho <kgh67811@naver.com>
2024-07-31 18:35:09 +0900

    Fix: Navbar 수정
    기존 Navbar를 Sidebar로 조정
    Sidebar toggle기능을 통해 View 확장

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import LoginPage from "./page/LoginPage";
 import JoinPage from "./page/JoinPage";
-import Navbar from "./components/Navbar";
+import Sidebar from "./components/Sidebar";
 import MainPage from "./page/MainPage";
 import CreatePage from "./page/CreatePage";
 import AllListPage from "./page/AllListPage";
@@ -13,7 +13,7 @@ import EditPage from "./page/EditPage";
 function App() {
   return (
     <BrowserRouter>
-      <Navbar />
+      <Sidebar />
       <Routes>
         <Route path="/" element={<MainPage />} />
         <Route path="/login" element={<LoginPage />} />
