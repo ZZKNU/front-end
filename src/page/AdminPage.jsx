4c4b06389fc7ff4a493cb0514baaa258ed8b7ad7
kimgho <kgh67811@naver.com>
2024-07-31 17:37:02 +0900

    Feat:Admin Page 관리자 페이지 작성중

---
+++ src/page/AdminPage.jsx
@@ -0,0 +1,10 @@
+/** Admin Page
+ *  기능 : 베스트 도전 게시물의 권한 관리
+ *  베스트 도전 글의 카테고리, ACCEPT / REJECT / PENDING
+ *  개요 : 각 ACCEPT , REJECT 버튼을 만들어서 해당 버튼을 클릭 시, API요청
+ * **/
+const AdminPage = () => {
+  return <h1>Admin</h1>;
+};
+
+export default AdminPage;
